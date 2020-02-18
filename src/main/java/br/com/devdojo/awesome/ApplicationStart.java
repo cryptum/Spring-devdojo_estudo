2ebe48c90983a5a508c4f88dfa03a9cde326baed
cryptum <danilovbvip@hotmail.com>
2020-02-18 14:51:50 -0300

    Aula 2 - @Component, @Autowired e @SpringBootApplication

--- src/main/java/br/com/devdojo/awesome/ApplicationStart.java
+++ src/main/java/br/com/devdojo/awesome/ApplicationStart.java
@@ -1,11 +1,13 @@
-package br.com.devdojo.awesome.start;
+package br.com.devdojo.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "br.com.devdojo.awesome.endpoint")
+
+@SpringBootApplication //  -> mesma coisa que  ->  @Configuration @ComponentScan @EnableAutoConfiguration
 public class ApplicationStart {
 
     public static void main(String[] args) {
